2a2e90d296ab4858bb522b933c95a0c669abccbc
wangzeping39 <542308148@qq.com>
2015-05-21 13:38:10 +0800

    first project
    此项目是本人第一个上传到github上面的项目

---
+++ src/main/java/com/ping/Hello.java
@@ -0,0 +1,15 @@
+package com.ping;
+/**
+ * 此工程主要是学习如何把本地代码上传到github上
+ * 还有编码问题
+ * @author lenovo
+ *
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+		System.out.println("是否可以上传中文");
+	}
+
+}

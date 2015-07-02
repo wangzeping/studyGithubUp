149ba34fc5fb37cedc22a65667cd8b95de71f0ee
wangzeping <wangzeping@users.noreply.github.com>
2015-07-02 15:51:30 +0800

    Update GitHubUp.java

--- src/main/java/com/ping/GitHubUp.java
+++ src/main/java/com/ping/GitHubUp.java
@@ -1,5 +1,9 @@
 package com.ping;
 public class GitHubUp{
+    public static void main(String[] args){
+        System.out.println("hello world....");
+    }
+    
     public void test(){
        System.out.println("aaaa....");
     }

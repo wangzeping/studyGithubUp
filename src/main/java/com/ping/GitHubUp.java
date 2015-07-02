e122b7e592bacec583316408ea8dc71ea606d088
wangzeping39 <542308148@qq.com>
2015-07-02 15:53:48 +0800

    bbb

--- src/main/java/com/ping/GitHubUp.java
+++ src/main/java/com/ping/GitHubUp.java
@@ -3,4 +3,8 @@ public class GitHubUp{
     public void test(){
        System.out.println("aaaa....");
     }
+    
+    public void test2(){
+    	System.out.println("I come to here...");
+    }
 }

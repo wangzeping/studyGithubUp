b1a09cb7820a4a59ff85a06b1050dc595e1e3418
wangzeping39 <542308148@qq.com>
2015-07-02 14:17:22 +0800

    修改

--- src/main/java/com/ping/entity/Person.java
+++ src/main/java/com/ping/entity/Person.java
@@ -1,32 +1,35 @@
-public class Person{
+package com.ping.entity;
 
-    private String name;
-    
-    private Integer age;
-    
-    private String address;
-    
-    public String getName(){
-        return name;
-      }
-    public void setName(String name){
-        this.name = name;
-      }
-      
-    public String age(){
-      return age;
-    }
-  
-  public void setAge(Integer age){
-      this.age = age;
-    }
-    
-  public String getAddress(){
-      return address;
-    }
-    
-  public void setAddress(String address){
-      this.address = address;
-    }  
+public class Person {
+
+	private String name;
+
+	private Integer age;
+
+	private String address;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
 }
